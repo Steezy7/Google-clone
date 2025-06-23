6b5013258a228a29d5ece282bd1242454afca031
Steezy7 <laioketoluwani17@gmail.com>
2025-06-23 13:37:54 +0100

    Add Body and footer compononents

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
 import HomeHeader from "@/components/HomeHeader"
+import HomeSeacrch from "@/components/HomeSearch"
+import Image from "next/image"
 
 export default function  HomePage(){
   return(
     <>
       <HomeHeader/>
+      <div className="flex flex-col items-center mt-40">
+        <Image 
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+        alt="google logo"
+        width={300} 
+        height={100}
+        priority
+        style={{width:"auto"}}
+        />
+
+        <HomeSeacrch/>
+      </div>
     </>
   )
 }
---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export default function CountryLookup(){
+    const [country, setCountry] = useState("Loading...");
+
+    useEffect(()=>{
+        const getCountry = async() => {
+            const response = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.NEXT_PUBLIC_IP_API_KEY}`)
+            const data = await response.json()
+            const countGot = data.country_name
+            setCountry(countGot);
+            console.log(countGot);
+        };
+        getCountry()
+    },[])
+    return(
+        <h1>{country}</h1>
+    )
+}
